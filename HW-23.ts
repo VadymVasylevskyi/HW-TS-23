4dd90f522b8b39a3fd0a3f45168dd9e2b5e801ef
VadymVasylevskyi <vadim.vasilevskyy@gmail.com>
2024-11-03 20:19:32 +0100

    HW-23

---
+++ HW-23.ts
@@ -0,0 +1,51 @@
+
+// Задание 1
+function delay(ms: number): Promise<string> {
+    return new Promise(resolve => setTimeout(() => resolve(`Completed after ${ms} ms`), ms));
+}
+
+async function processPromisesSequentially() {
+    const result1 = await delay(1000); 
+    console.log(result1);
+
+    const result2 = await delay(2000);
+    console.log(result2);
+
+    const result3 = await delay(1500); 
+    console.log(result3);
+}
+
+// Задание 2
+async function processStrings(strings: string[]): Promise<string[]> {
+    const promises = strings.map(str => 
+        new Promise<string>((resolve) => {
+            setTimeout(() => resolve(str.toUpperCase()), 1000);
+        })
+    );
+    return await Promise.all(promises);
+}
+
+// Задание 3
+async function handleErrorInPromises() {
+    const promise1 = delay(1000);
+    const promise2 = delay(2000);
+    const promise3 = new Promise<string>((_, reject) => setTimeout(() => reject('Error in Promise 3'), 1500));
+
+    try {
+        const results = await Promise.all([promise1, promise2, promise3]);
+        console.log(results);
+    } catch (error) {
+        console.error(`Caught error: ${error}`);
+    }
+}
+
+// Задание 4
+async function dynamicDelay(numbers: number[]) {
+    const promises = numbers.map(num =>
+        new Promise<string>(resolve => setTimeout(() => resolve(`Resolved after ${num} ms`), num))
+    );
+    const results = await Promise.all(promises);
+    console.log(results);
+}
+
+
